f58421272fa3ac7e5ac0614036a9d8c0f36a808f
Anna K <anna.glushakova07@gmail.com>
2023-05-04 17:52:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amountTicket = 10_000;
+        int amountBonus = 20;
+
+        int bonus = amountTicket/amountBonus;
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
